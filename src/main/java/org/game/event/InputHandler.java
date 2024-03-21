7d9f0e42234dfd847d8eefbee1c44bb1192087b9
viaicamon <viaicamon28@gmail.com>
2024-03-21 23:26:18 +0700

    Input Handle

--- src/main/java/org/game/event/InputHandler.java
+++ src/main/java/org/game/event/InputHandler.java
@@ -5,23 +5,50 @@ import java.awt.event.KeyListener;
 
 public class InputHandler implements KeyListener {
 
-    public boolean upPressed, downPressed, leftPressed, rightPressed;
+    private boolean upPressed, downPressed, leftPressed, rightPressed;
+    
+    public boolean isUpPressed()    {   return upPressed;   }
+    public boolean isDownPressed()  {   return downPressed; }
+    public boolean isLeftPressed()  {   return leftPressed; }
+    public boolean isRightPressed() {   return rightPressed;}
+
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
-        
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_UP:    upPressed    = true; break;
+            case KeyEvent.VK_W:     upPressed    = true; break;
+            case KeyEvent.VK_DOWN:  downPressed  = true; break;
+            case KeyEvent.VK_S:     downPressed  = true; break;
+            case KeyEvent.VK_LEFT:  leftPressed  = true; break;
+            case KeyEvent.VK_A:     leftPressed  = true; break;
+            case KeyEvent.VK_RIGHT: rightPressed = true; break;
+            case KeyEvent.VK_D:     rightPressed = true; break;
+        }
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
-        
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_UP:    upPressed    = false; break;
+            case KeyEvent.VK_W:     upPressed    = false; break;
+            case KeyEvent.VK_DOWN:  downPressed  = false; break;
+            case KeyEvent.VK_S:     downPressed  = false; break;
+            case KeyEvent.VK_LEFT:  leftPressed  = false; break;
+            case KeyEvent.VK_A:     leftPressed  = false; break;
+            case KeyEvent.VK_RIGHT: rightPressed = false; break;                     
+            case KeyEvent.VK_D:     rightPressed = false; break;
+        }
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
-        
+        //  Đã có keyPressed và keyReleased để bắt sự kiện
     }
     
 }
